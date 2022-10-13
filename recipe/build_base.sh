677ed75db7d3d1bcacc685e78c28d8edd21fd012
Avalon <avalon@anaconda.com>
2022-10-13 13:40:10 -0700

    clean up build_base.sh ppc64le if statements

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -47,13 +47,10 @@ if [[ ${target_platform} == linux-aarch64 ]]; then
   _OPTIMIZED=no
 fi
 
-# ppc64le cdt need to be rebuilt with files in powerpc64le-conda-linux-gnu instead of powerpc64le-conda_cos7-linux-gnu. In the mean time:
-if [ "$(uname -m)" = "ppc64le" ]; then
-  cp --force --archive --update --link $BUILD_PREFIX/powerpc64le-conda_cos7-linux-gnu/. $BUILD_PREFIX/powerpc64le-conda-linux-gnu
-fi
-
 if [[ ${target_platform} == linux-ppc64le ]]; then
   _OPTIMIZED=no
+  # ppc64le cdt need to be rebuilt with files in powerpc64le-conda-linux-gnu instead of powerpc64le-conda_cos7-linux-gnu. In the mean time:
+  cp --force --archive --update --link $BUILD_PREFIX/powerpc64le-conda_cos7-linux-gnu/. $BUILD_PREFIX/powerpc64le-conda-linux-gnu
 fi
 
 declare -a _dbg_opts
