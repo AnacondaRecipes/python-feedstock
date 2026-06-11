0bc96a6709394874f954f0d7d8d1f2c1b8c246cb
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-10 20:14:41 -0600

    Simplify test.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -98,3 +98,8 @@ import ssl
 print('OPENSSL_VERSION:', ssl.OPENSSL_VERSION)
 CONDA_OPENSSL_VERSION = os.getenv("openssl")
 assert CONDA_OPENSSL_VERSION in ssl.OPENSSL_VERSION
+# xref https://github.com/conda-forge/openssl-feedstock/issues/237
+pem = ssl.get_server_certificate(("pypi.org", 443))
+der = ssl.PEM_cert_to_DER_cert(pem)
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ctx.load_verify_locations(cadata=der)
